c6fd4d2f7f85f295fb0fae6766d2ac99ab0cac3f
Evelyn Craven <evelyn.mc.314@gmail.com>
2023-11-30 12:50:09 -0500

    article audio edit

--- src/templates/article.js
+++ src/templates/article.js
@@ -9,38 +9,39 @@ import "../styles/articles.css";
 // This Function will find all the article components on the drupal site, and create a page
 // on the gatsby site for each component
 
-function Article({data}) {
+function Article({ data }) {
     const post = data.nodeArticle;
+    const audio_url = post.relationships.field_audio.relationships.field_media_audio_file.uri.url;
 
     return (
         <Layout>
             <div className="articleContainer">
-            {post.relationships.field_audio && 
-                <div className="articleImageContainer">
-                    <img className="articleImage" src={post.relationships.field_image.localFile.url}></img>
-                    <div className="articleTitleInfo">
-                        <h1>{post.title}</h1>
-                        <h3>By {post.field_author}</h3>
-                        <audio className="articleAudio" src = {post.relationships.field_audio != null ? "https://www.empathybytes.library.gatech.edu" + post.relationships.field_audio.path.alias : null} controls>
-                        </audio>
+                {post.relationships.field_audio &&
+                    <div className="articleImageContainer">
+                        <img className="articleImage" src={post.relationships.field_image.localFile.url}></img>
+                        <div className="articleTitleInfo">
+                            <h1>{post.title}</h1>
+                            <h3>By {post.field_author}</h3>
+                            <audio className="articleAudio" src={post.relationships.field_audio != null ? "https://www.empathybytes.library.gatech.edu" + audio_url : null} controls>
+                            </audio>
+                        </div>
                     </div>
+                }
+
+                {post.field_video_url ?
+                    <iframe className="articleVideo" width="887" height="499"
+                        src={post.field_video_url}>
+                    </iframe> : null
+                }
+
+
+                <div className="articleText" dangerouslySetInnerHTML={{ __html: post.body.processed }}></div>
+
+
+                <div class="clicker" tabindex="1">View Transcript</div>
+                <div class="hiddendiv">
+                    <div className="articleText" dangerouslySetInnerHTML={{ __html: post.field_transcript.processed }}></div>
                 </div>
-            }
-
-            { post.field_video_url ?
-                <iframe className="articleVideo" width="887" height="499" 
-                    src={post.field_video_url}>
-                </iframe> : null
-            }
-
-
-            <div className="articleText" dangerouslySetInnerHTML={{ __html: post.body.processed }}></div>
-            
-            
-            <div class="clicker" tabindex="1">View Transcript</div>
-            <div class="hiddendiv">
-                <div className="articleText" dangerouslySetInnerHTML={{ __html: post.field_transcript.processed }}></div>
-            </div>
             </div>
         </Layout>
     );
@@ -74,13 +75,14 @@ export const query = graphql`
                 }
             }
             field_audio {
-                path {
-                    alias
+                relationships {
+                  field_media_audio_file {
+                    uri {
+                      url
+                    }
+                  }
                 }
-                internal {
-                    contentFilePath
-                }
-            }
+              }
         }
     }
 }
