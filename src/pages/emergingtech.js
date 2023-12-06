b847d7aedd4e842ed86ca331f80535951fa48f61
aduodu <angieduodu3@gmail.com>
2023-12-06 08:22:51 -0500

    Removed glitching football 3d model.

--- src/pages/emergingtech.js
+++ src/pages/emergingtech.js
@@ -156,25 +156,6 @@ function EmergingTech() {
                             </Suspense>
                         </Canvas>
                     </Grid>
-
-                        <Grid xs={6} className="vr-container-2 b-2">
-                            <Canvas>
-                                <Suspense>
-                                    <Converse scale={6} />
-
-                                    {/*PLEASE READ (6/10/2023)
-                                        There is a compatibility issue with this Environment component and the new version of three.js, I think it should be resolved soon though.
-                                        For now, I have inlcuded an ambientLight component instead. When this issue is resolved, feel free to add the Environment component back in.
-                                    - Jacob Amin */}
-
-                                    {/* <Environment /> */}
-                                    <ambientLight />
-                                    <OrbitControls />
-                                </Suspense>
-                            </Canvas>
-                        </Grid>
-
-
                     
                 </Grid>
 
